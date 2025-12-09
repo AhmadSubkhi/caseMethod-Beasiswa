ff127f3f28276dea8ec1132596080f773fce6a62
mov1cc <imovic004@gmail.com>
2025-12-09 21:06:34 +0700

    Deklarasi Array

--- caseMethodBeasiswa.java
+++ caseMethodBeasiswa.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class caseMethodBeasiswa {
     public static void main(String[] args) {
-        
+
+        String[] namaMahasiswa = new String[100];
+        long[] NIM = new long[100];
+        float[] IPK = new float[100];
+        String[] jenisBeasiswa = new String[100];
+        int[] penghasilanOrtu = new int[100];
+        int totalData = 0;
     }
 }
